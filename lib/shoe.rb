29bf2bb4c225fa03dab3d1638b73c55f637e2f83
Shamel Lakin <christlife4u@gmail.com>
2019-09-26 14:31:39 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,11 +2,11 @@ class Shoe
   attr_accessor :brand, :color, :size, :material, :condition
   attr_reader :title
  
-  def initialize(title)
-    @title = title
+  def initialize(cobble)
+    @cobble = cobble
   end
  
-  def turn_page
+  def cobble
     puts "Flipping the page...wow, you read fast!"
   end
  
