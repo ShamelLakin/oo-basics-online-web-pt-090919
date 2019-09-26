400966aaeebdd069f54ccf40e748014288d85de3
Shamel Lakin <christlife4u@gmail.com>
2019-09-26 14:21:46 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -39,10 +39,9 @@ class Shoe
     @condition
   end 
   
-  def cobble(condition)
-    if condition == true
+  def cobble
+    self.condition = "new"
    puts "Your shoe is as good as new!"
-   end
   end 
   
 end
